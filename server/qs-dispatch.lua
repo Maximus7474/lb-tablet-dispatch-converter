572d81c10fa73e165bcd4abcfa6c1d9a9bea337d
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-27 20:21:42 +0100

    feat(server): qs-dispatch compatibility

---
+++ server/qs-dispatch.lua
@@ -0,0 +1,20 @@
+AddEventHandler('qs-dispatch:server:CreateDispatchCall', function (data)
+
+    local priority = "medium"
+    if data.flashes then
+        priority = "high"
+    end
+
+    exports["lb-tablet"]:AddDispatch({
+        priority = priority,
+        code = data?.callCode.code or '',
+        title = data?.callCode.snippet or 'Crime Reported',
+        description = data.message or '',
+        location = {
+          label = data?.callCode.snippet or "Call Origin",
+          coords = vector2(data?.callLocation.x or 0, data?.callLocation.y or 0)
+        },
+        time = data?.blip.time and math.floor(data.blip.time / 1000) or 100,
+        job = "police"
+    })
+end)
