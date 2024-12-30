4643332984ab3d2f874c1a7831f285ad6c15f80d
Maximus7474 <the.maximus.7474@gmail.com>
2024-12-30 22:24:35 +0100

    fix(utils): exportHandler

--- utils/exportHandler.lua
+++ utils/exportHandler.lua
@@ -2,8 +2,8 @@
     Obtained from https://github.com/overextended/ox_target/blob/main/client/compat/qtarget.lua#L1C1-L5C4
 ]]
 
-function ExportHandler(exportName, func)
-    AddEventHandler(('__cfx_export_qtarget_%s'):format(exportName), function(setCB)
+function ExportHandler(resource, exportName, func)
+    AddEventHandler(('__cfx_export_%s_%s'):format(resource, exportName), function(setCB)
         setCB(func)
     end)
 end
