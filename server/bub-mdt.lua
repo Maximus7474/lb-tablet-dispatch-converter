f835f675d18b732af5ab930fffe872402a5618aa
Maximus7474 <94017712+Maximus7474@users.noreply.github.com>
2024-12-30 22:35:54 +0100

    chore(bub-mdt): added export handler

--- server/bub-mdt.lua
+++ server/bub-mdt.lua
@@ -13,3 +13,19 @@ RegisterServerEvent("mdt:server:CreateCall", function(data)
         fields = data?.info or {}
     })
 end)
+
+ExportHandler("bub-mdt", "CustomAlert", function(data)
+    exports["lb-tablet"]:AddDispatch({
+        priority = data?.isEmergency and "high" or "medium",
+        code = data?.code or '',
+        title = data?.offense or T("DEFAULT.TITLE"),
+        description = "",
+        location = {
+            label = data?.location or data?.offense or T("DEFAULT.LOCATION_LABEL"),
+            coords = vector2(data?.coords.x or 0, data?.coords.y or 0)
+        },
+        time = CONFIG['default-time'],
+        job = "police",
+        fields = data?.info or {}
+    })
+end)
